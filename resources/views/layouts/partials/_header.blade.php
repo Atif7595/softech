99eb3ea12f085a32d87eb880c5e44ad555f2a136
Atif7595 <atifsaeed.5243@gmail.com>
2023-10-04 21:40:34 +0500

    admin pannel

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -4387,7 +4387,7 @@
                 </div>
                 <!--end::Notifications-->
                 <!--begin::Chat-->
-                <div class="app-navbar-item ms-1 ms-lg-3">
+                {{-- <div class="app-navbar-item ms-1 ms-lg-3">
                     <!--begin::Menu wrapper-->
                     <div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px position-relative" id="kt_drawer_chat_toggle">
                         <!--begin::Svg Icon | path: icons/duotune/communication/com012.svg-->
@@ -4402,7 +4402,7 @@
                         <span class="bullet bullet-dot bg-success h-6px w-6px position-absolute translate-middle top-0 start-50 animation-blink"></span>
                     </div>
                     <!--end::Menu wrapper-->
-                </div>
+                </div> --}}
                 <!--end::Chat-->
                 <!--begin::Quick links-->
                 <div class="app-navbar-item ms-1 ms-lg-3">
